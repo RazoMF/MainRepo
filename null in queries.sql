454cc35386de5624153cd9a7293d1f85c92791f8
Alexander M Maslov <maslov.am.msu@gmail.com>
2018-11-09 16:29:34 +0300

    ''

---
+++ null in queries.sql
@@ -0,0 +1,40 @@
+select 1+null, 1-null,1*null,1/null
+from dual;
+
+with 
+smpl 
+  as
+(
+  select 1 a
+  from dual
+  union all
+  select 2
+  from dual
+  union all
+  select NULL
+  from dual
+)
+select min(a), max(a), avg(a), sum(a)
+from smpl;
+
+with 
+smpl 
+  as
+(
+  select 1 a
+  from dual
+  union all
+  select 2
+  from dual
+  union all
+  select NULL
+  from dual
+)
+select a
+from smpl
+where null != 0 --null is null -true, null = null -false, null != null -false, null > 0 -false, null < 0 -false, null != 0 -false
+order by a nulls last;
+
+
+
+
