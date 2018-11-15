72e36253d9447f9d87e43782b43783bf31c3a31c
Alexander M Maslov <maslov.am.msu@gmail.com>
2018-11-15 16:59:33 +0300

    some edditions

--- PL-SQL Scripts/Create tables Emp and Dept. Alter tables.sql
+++ PL-SQL Scripts/Create tables Emp and Dept. Alter tables.sql
@@ -37,6 +37,8 @@ alter table emp add constraint emp_pk primary key(empno);
 alter table dept add constraint dept_pk primary key(deptno);
 alter table emp add constraint emp_fk_dept foreign key(deptno) references dept;
 alter table emp add constraint emp_fk_emp foreign key(mgr) references emp; --добавим ключ 
+/*Можно создать индекс, но не имеет смысла для первичного ключа empno 
+create index emp_empno_idx on emp(empno); */
 
 
 /*
--- PL-SQL Scripts/Hints.sql
+++ PL-SQL Scripts/Hints.sql
@@ -2,3 +2,22 @@
 SELECT /*+ FIRST_ROWS(20) */ * ---Оптимизирует запрос для выборки первых строк
   FROM bigtab
   WHERE bigtab.id != 10;
+ 
+--Join по хэшу
+select /*+ USE_HASH(t2 t1) */ * 
+from bigtab4 t1 
+join bigtab t2 on t2.id = t1.id;
+  
+--Медленный запрос. Стоимость запроса = 1888.
+select *
+from bigtab4 t1 
+where id = 1000;
+
+--Создадим индекс
+create index bigtab4_id_idx on bigtab4(id);
+
+--Теперь стоимость запроса = 7.
+select /*+ Index(t1 bigtab4_id_idx) */ *
+from bigtab4 t1 
+where id = 1000;
+
